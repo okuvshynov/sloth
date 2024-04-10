2354bd88ceb0a2b38613537ebf9ac1c4ea2ef2c3
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-04-10 14:37:34 -0400

    no dist for 7x8

---
+++ mixtral8x7b_no_dist.py
@@ -0,0 +1,530 @@
+# modified version of one_file_ref.py by mistral-ai, Apache Licence 2.0
+
+import json
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Optional, Tuple
+
+import torch
+from sentencepiece import SentencePieceProcessor
+from simple_parsing.helpers import Serializable
+from torch import nn
+import sys
+from blackbox import BlackboxDisk
+
+@dataclass
+class MoeArgs(Serializable):
+    num_experts: int
+    num_experts_per_tok: int
+
+@dataclass
+class ModelArgs(Serializable):
+    dim: int
+    n_layers: int
+    head_dim: int
+    hidden_dim: int
+    n_heads: int
+    n_kv_heads: int
+    norm_eps: float
+    vocab_size: int
+    moe: MoeArgs
+
+    max_batch_size: int = 0
+    max_seq_len: int = 0
+    compute_dtype: torch.dtype = torch.float16
+    frozen_dtype: torch.dtype = torch.float16
+    served_model_path: str = '' # relative path by default
+    cached_data_path: str = ''  # relative path by default
+    init_frozen: bool = True
+
+
+def repeat_kv(keys: torch.Tensor, values: torch.Tensor, repeats: int):
+    keys = torch.repeat_interleave(keys, repeats=repeats, dim=2)
+    values = torch.repeat_interleave(values, repeats=repeats, dim=2)
+    return keys, values
+
+
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
+    ndim = x.ndim
+    assert 0 <= 1 < ndim
+    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    return freqs_cis.view(shape)
+
+def apply_rotary_emb(
+    xq: torch.Tensor,
+    xk: torch.Tensor,
+    freqs_cos: torch.Tensor,
+    freqs_sin: torch.Tensor
+) -> Tuple[torch.Tensor, torch.Tensor]:
+
+    # reshape xq and xk to match the complex representation
+    xq_r, xq_i = xq.float().reshape(xq.shape[:-1] + (-1, 2)).unbind(-1)
+    xk_r, xk_i = xk.float().reshape(xk.shape[:-1] + (-1, 2)).unbind(-1)
+
+    # reshape freqs_cos and freqs_sin for broadcasting
+    freqs_cos = reshape_for_broadcast(freqs_cos, xq_r)
+    freqs_sin = reshape_for_broadcast(freqs_sin, xq_r)
+
+    # apply rotation using real numbers
+    xq_out_r = xq_r * freqs_cos - xq_i * freqs_sin
+    xq_out_i = xq_r * freqs_sin + xq_i * freqs_cos
+    xk_out_r = xk_r * freqs_cos - xk_i * freqs_sin
+    xk_out_i = xk_r * freqs_sin + xk_i * freqs_cos
+
+    # flatten last two dimensions
+    xq_out = torch.stack([xq_out_r, xq_out_i], dim=-1).flatten(3)
+    xk_out = torch.stack([xk_out_r, xk_out_i], dim=-1).flatten(3)
+
+    return xq_out.type_as(xq), xk_out.type_as(xk)
+
+
+class Attention(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.args = args
+
+        self.n_heads: int = args.n_heads
+        self.n_kv_heads: int = args.n_kv_heads
+
+        self.repeats = self.n_heads // self.n_kv_heads
+
+        self.scale = self.args.head_dim**-0.5
+
+        self.wq = nn.Linear(args.dim, args.n_heads * args.head_dim, bias=False)
+        self.wk = nn.Linear(args.dim, args.n_kv_heads * args.head_dim, bias=False)
+        self.wv = nn.Linear(args.dim, args.n_kv_heads * args.head_dim, bias=False)
+        self.wo = nn.Linear(args.n_heads * args.head_dim, args.dim, bias=False)
+        self._cache_k: Optional[torch.Tensor] = None
+        self._cache_v: Optional[torch.Tensor] = None
+
+    def get_caches(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        dtype, device = x.dtype, x.device
+        assert x.shape[0] <= self.args.max_batch_size
+        assert x.shape[1] <= self.args.max_seq_len
+        if self._cache_k is None:
+            self._cache_k = torch.empty(
+                (
+                    self.args.max_batch_size,
+                    self.args.max_seq_len,
+                    self.n_kv_heads,
+                    self.args.head_dim,
+                ),
+                dtype=dtype,
+                device=device,
+            )
+        if self._cache_v is None:
+            self._cache_v = torch.empty(
+                (
+                    self.args.max_batch_size,
+                    self.args.max_seq_len,
+                    self.n_kv_heads,
+                    self.args.head_dim,
+                ),
+                dtype=dtype,
+                device=device,
+            )
+        return self._cache_k, self._cache_v
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        freqs_cos: torch.Tensor,
+        freqs_sin: torch.Tensor,
+        positions: torch.Tensor,
+        mask: Optional[torch.Tensor],
+    ) -> torch.Tensor:
+        bsz, seqlen, _ = x.shape
+
+        cache_k, cache_v = self.get_caches(x)
+
+        xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
+        xq = xq.view(bsz, seqlen, self.n_heads, self.args.head_dim)
+        xk = xk.view(bsz, seqlen, self.n_kv_heads, self.args.head_dim)
+        xv = xv.view(bsz, seqlen, self.n_kv_heads, self.args.head_dim)
+        xq, xk = apply_rotary_emb(xq, xk, freqs_cos, freqs_sin)
+
+        # The cache is a rotating buffer
+        scatter_pos = (positions % self.args.max_seq_len)[None, :, None, None]
+        scatter_pos = scatter_pos.repeat(bsz, 1, self.n_kv_heads, self.args.head_dim)
+        cache_k[:bsz].scatter_(dim=1, index=scatter_pos, src=xk)
+        cache_v[:bsz].scatter_(dim=1, index=scatter_pos, src=xv)
+
+        if positions.shape[0] > 1:
+            # prefill
+            key, value = repeat_kv(xk, xv, self.repeats)
+        else:
+            assert mask is None
+            cur_pos = int(positions[-1].item() + 1)
+            key, value = repeat_kv(
+                cache_k[:bsz, :cur_pos, ...],
+                cache_v[:bsz, :cur_pos, ...],
+                self.repeats,
+            )
+
+        query = xq.transpose(1, 2)
+        key = key.transpose(1, 2)
+        value = value.transpose(1, 2)
+        # scores : [bsz, n_heads, seqlen | 1, seqlen]
+        scores = torch.matmul(query, key.transpose(2, 3)) * self.scale
+
+        if mask is not None:
+            scores += mask[None, None, ...]
+
+        scores = scores.float()
+        scores = nn.functional.softmax(scores, dim=-1).type_as(query)
+        output = torch.matmul(scores, value)  # (bs, n_local_heads, slen, head_dim)
+        output = output.transpose(1, 2).contiguous().view(bsz, seqlen, -1)
+        return self.wo(output)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.w1 = nn.Linear(args.dim, args.hidden_dim, bias=False)
+        self.w2 = nn.Linear(args.hidden_dim, args.dim, bias=False)
+        self.w3 = nn.Linear(args.dim, args.hidden_dim, bias=False)
+
+    def forward(self, x) -> torch.Tensor:
+        return self.w2(nn.functional.silu(self.w1(x)) * self.w3(x))
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight
+
+
+class MoeLayer(nn.Module):
+    def __init__(self, experts: List[nn.Module], gate: nn.Module, moe_args: MoeArgs):
+        super().__init__()
+        assert len(experts) > 0
+        self.experts = nn.ModuleList(experts)
+        self.gate = gate
+        self.args = moe_args
+        self.selected_freq = [0 for i in range(len(experts))]
+
+    def forward(self, inputs: torch.Tensor):
+        inputs_squashed = inputs.view(-1, inputs.shape[-1])
+        gate_logits = self.gate(inputs_squashed)
+        weights, selected_experts = torch.topk(
+            gate_logits, self.args.num_experts_per_tok
+        )
+        for ex in selected_experts.view(-1).tolist():
+            self.selected_freq[ex] += 1
+        
+        weights = nn.functional.softmax(
+            weights,
+            dim=1,
+            dtype=torch.float,
+        ).type_as(inputs)
+        results = torch.zeros_like(inputs_squashed)
+        for i, expert in enumerate(self.experts):
+            batch_idx, nth_expert = torch.where(selected_experts == i)
+            if weights[batch_idx, nth_expert, None].numel() > 0:
+                tmp = weights[batch_idx, nth_expert, None] * expert(
+                    inputs_squashed[batch_idx]
+                )
+                results[batch_idx] += tmp
+        return results.view_as(inputs)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.n_heads = args.n_heads
+        self.dim = args.dim
+        self.attention = Attention(args)
+        self.feed_forward = MoeLayer(
+            experts=[BlackboxDisk(FeedForward(args=args), args) for _ in range(args.moe.num_experts)],
+            gate=nn.Linear(args.dim, args.moe.num_experts, bias=False),
+            moe_args=args.moe,
+        )
+        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
+        self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
+        self.args = args
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        freqs_cos: torch.Tensor,
+        freqs_sin: torch.Tensor,
+        positions: torch.Tensor,
+        mask: Optional[torch.Tensor],
+    ) -> torch.Tensor:
+        r = self.attention.forward(self.attention_norm(x), freqs_cos, freqs_sin, positions, mask)
+        
+        h = x + r
+        r = self.feed_forward.forward(self.ffn_norm(h))
+        out = h + r
+        return out
+
+
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device)
+    freqs = torch.outer(t, freqs).float()
+    freqs_cos = torch.cos(freqs)
+    freqs_sin = torch.sin(freqs)
+    return freqs_cos, freqs_sin
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        args: ModelArgs,
+        pipeline_rank: int = 0,
+    ):
+        super().__init__()
+        self.args = args
+        self.vocab_size = args.vocab_size
+        assert self.vocab_size > 0
+        self._precomputed_freqs_cos: Optional[torch.Tensor] = None
+        self._precomputed_freqs_sin: Optional[torch.Tensor] = None
+
+        # Modules specific to some ranks:
+        self.tok_embeddings: Optional[nn.Embedding] = None
+        self.norm: Optional[RMSNorm] = None
+        self.output: Optional[nn.Linear] = None
+        self.tok_embeddings = nn.Embedding(args.vocab_size, args.dim)
+        self.norm = RMSNorm(args.dim, eps=args.norm_eps)
+        self.output = nn.Linear(args.dim, args.vocab_size, bias=False)
+
+        # Initialize all layers but slice off those not of this rank.
+        layers = [TransformerBlock(args=args) for _ in range(args.n_layers)]
+        self.layers = nn.ModuleDict({str(i): layers[i] for i in range(args.n_layers)})
+
+    @property
+    def dtype(self) -> torch.dtype:
+        return next(self.parameters()).dtype
+
+    @property
+    def device(self) -> torch.device:
+        return next(self.parameters()).device
+
+    @property
+    def freqs_cis(self) -> torch.Tensor:
+        if self._precomputed_freqs_cos is None:
+            self._precomputed_freqs_cos, self._precomputed_freqs_sin = precompute_freqs_cis(
+                self.args.head_dim, 128_000
+            )
+        if self._precomputed_freqs_cos.device != self.device:
+            self._precomputed_freqs_cos = self._precomputed_freqs_cos.to(
+                device=self.device
+            )
+        if self._precomputed_freqs_sin.device != self.device:
+            self._precomputed_freqs_sin = self._precomputed_freqs_sin.to(
+                device=self.device
+            )
+        return self._precomputed_freqs_cos, self._precomputed_freqs_sin
+
+    def forward(
+        self,
+        input_ids: torch.Tensor,
+        positions: torch.Tensor,
+    ):
+        freqs_cos, freqs_sin = self.freqs_cis
+        freqs_cos = freqs_cos[positions]
+        freqs_sin = freqs_sin[positions]
+
+        (bsz, seqlen) = input_ids.shape
+        num_toks = bsz * seqlen
+
+        assert self.tok_embeddings is not None
+        h = self.tok_embeddings(input_ids)
+        assert h.shape == (bsz, seqlen, self.args.dim)
+        assert h.dtype == self.dtype
+
+        mask: Optional[torch.Tensor] = None
+        if input_ids.shape[1] > 1:
+            tensor = torch.full(
+                (seqlen, seqlen),
+                dtype=h.dtype,
+                fill_value=1,
+                device=h.device,
+            )
+            mask = torch.log(torch.tril(tensor, diagonal=0)).to(h.dtype)
+
+        for layer in self.layers.values():
+            h = layer(h, freqs_cos, freqs_sin, positions, mask)
+
+        assert self.output is not None
+        assert self.norm is not None
+        outs = self.output(self.norm(h))
+
+        return outs.float()
+
+    def load_state_dict(self, state_dict, *args, **kwargs):
+        super().load_state_dict(state_dict, *args, **kwargs)
+
+    @staticmethod
+    def from_folder(
+        folder: Path,
+        max_batch_size: int,
+        max_seq_len: int,
+        device="mps",
+        dtype=torch.float16,
+    ) -> "Transformer":
+        with open(folder / "params.json", "r") as f:
+            model_args = ModelArgs.from_dict(json.load(f))
+        model_args.max_batch_size = max_batch_size
+        model_args.max_seq_len = max_seq_len
+        logging.info(f'creating the model from {folder}')
+        model = Transformer(
+            model_args,
+        ).to(device=device, dtype=dtype)
+        logging.info(f'loading (mmap) state dict')
+        loaded = torch.load(str(folder / "consolidated.00.pth"), mmap=True)
+        logging.info(f'applying state dict')
+        model.load_state_dict(loaded, assign=True, strict=False)
+        logging.info(f'loading MoE ff')
+
+        ## load ff separately
+        for i_str, layer in model.layers.items():
+            logging.info(f'loading layer.{i_str}')
+            for j, expert in enumerate(layer.feed_forward.experts):
+                inner = expert.loaded_inner()
+                prefix = f'layers.{i_str}.feed_forward.experts.{j}.'
+                ff_dict = {k[len(prefix):]: v for k, v in loaded.items() if k.startswith(prefix)}
+                inner.load_state_dict(ff_dict)
+                expert.save(inner)
+
+        return model.to(device=device, dtype=dtype)
+
+
+class Tokenizer:
+    def __init__(self, model_path: str):
+        assert Path(model_path).exists(), model_path
+        self._model = SentencePieceProcessor(model_file=model_path)
+        assert self._model.vocab_size() == self._model.get_piece_size()
+
+    @property
+    def eos_id(self) -> int:
+        return self._model.eos_id()
+
+    @property
+    def pad_id(self) -> int:
+        return self._model.pad_id()
+
+    def encode(self, s: str) -> List[int]:
+        return [self._model.bos_id(), *self._model.encode(s)]
+
+    def decode(self, t: List[int]) -> str:
+        return self._model.decode(t)
+
+
+@torch.no_grad()
+def generate(
+    prompts: List[str], model: Transformer, tokenizer: Tokenizer, max_tokens: int
+):
+    encoded_prompts = [tokenizer.encode(prompt) for prompt in prompts]
+    prompt_lens = [len(x) for x in encoded_prompts]
+    min_prompt_len = min(prompt_lens)
+    max_prompt_len = max(prompt_lens)
+
+    input_tokens = torch.full(
+        (len(prompts), max_prompt_len),
+        tokenizer.pad_id,
+        dtype=torch.long,
+        device="mps",
+    )
+    for i, encoded in enumerate(encoded_prompts):
+        input_tokens[i, : len(encoded)] = torch.tensor(encoded).to(input_tokens)
+    input_mask = input_tokens != tokenizer.pad_id
+
+    # pre-fill
+    positions = torch.arange(0, min_prompt_len).to("mps")
+    logits = model.forward(input_tokens[:, :min_prompt_len], positions)
+    logprobs = nn.functional.log_softmax(logits, dim=-1)
+
+    # decode
+    generated = []
+    all_logprobs = [
+        logprobs[:, :-1, :]
+        .gather(2, input_tokens[:, 1:min_prompt_len, None])
+        .squeeze(-1),
+    ]
+    for cur_pos in range(min_prompt_len, max_tokens):
+        next_token = torch.argmax(logprobs[:, -1, :], dim=-1)
+        if cur_pos < input_mask.shape[1]:
+            next_token = torch.where(
+                input_mask[:, cur_pos], input_tokens[:, cur_pos], next_token
+            )
+        all_logprobs.append(
+            logprobs[:, -1, :].gather(1, next_token[:, None]),
+        )
+        generated.append(next_token[:, None])
+        logging.info(f'next token: {tokenizer.decode(generated[-1].tolist())}')
+        logits = model.forward(
+            next_token[:, None], torch.LongTensor([cur_pos]).to(next_token)
+        )
+        logprobs = nn.functional.log_softmax(logits, dim=-1)
+
+    all_logprobs_merged = torch.cat(all_logprobs, 1)
+    res = []
+    if max_tokens > 0:
+        generated = torch.cat(generated, 1)
+        for i, x in enumerate(encoded_prompts):
+            res.append(tokenizer.decode(x[:min_prompt_len] + generated[i].tolist()))
+    return res, all_logprobs_merged
+
+def gen_prompts(k):
+    base = [
+        "Explain the concept of machine learning in simple terms.",
+        "What are the benefits of renewable energy sources?",
+        "Write a short poem about the sea.",
+        "Summarize the plot of 'Pride and Prejudice'.",
+        "Provide three tips for effective time management.",
+        "Describe the process of photosynthesis.",
+        "What are the main differences between Python 2 and Python 3?",
+        "How does blockchain technology work?",
+        "Explain the significance of the Turing Test.",
+        "What are some effective strategies for learning a new language?",
+        "Briefly describe the history of the Internet.",
+        "What is the theory of relativity?",
+        "Give an example of a healthy meal plan for a day.",
+        "Explain the importance of biodiversity.",
+        "How can one improve their critical thinking skills?",
+        "Describe the impact of social media on society."
+    ]
+    
+    prompts = ["[INST] " + s + " [/INST]" for s in base]
+    n = len(prompts)
+
+    return [prompts[i % n] for i in range(k)]
+
+def demo(model_path: str, max_tokens: int = 1024, batch_size=16):
+    should_print = True
+
+    tokenizer = Tokenizer(str(Path(model_path) / "tokenizer.model"))
+    transformer = Transformer.from_folder(
+        Path(model_path),
+        max_batch_size=batch_size,
+        max_seq_len=max_tokens,
+    )
+
+    res, logprobs = generate(
+        gen_prompts(batch_size),
+        transformer,
+        tokenizer,
+        max_tokens=max_tokens,
+    )
+    if should_print:
+        for x, l in zip(res, logprobs):
+            print(x)
+            logging.debug("logprobs: %s", l)
+            print("=====================")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+    demo(sys.argv[1], batch_size=1)
