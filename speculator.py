b4ec18f78e635e34f6ce70ec2e7d3d564c1a4845
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-04-16 11:10:28 -0400

    measure latency correctly
    because of lazy eval it was messed up

--- speculator.py
+++ speculator.py
@@ -48,74 +48,75 @@ class Speculator:
         self.cache_len = 0
         self.tokens = []
 
-
     def handle_query(self, request):
         # request must have:
         # session_id. If changed, all previous session data is gone
         # tokens -- either initial prompt or incremental update within the session
         # max_tokens -- how many more tokens to generate after the current incremental update.
+        with ft.Timer("handle_query_latency") as _:
+            self.max_tokens = request.get('max_tokens', default_max_tokens)
 
-        self.max_tokens = request.get('max_tokens', default_max_tokens)
+            if self.session_id is None or self.session_id != request['session_id']:
+                # starting new session
+                self.session_id = request['session_id']
+                logging.info(f"starting session {self.session_id}")
+                self.tokens = request['tokens'][:]
+                self.cache = [None for _ in self.model.layers]
+                self.cache_len = 0
+            else:
+                # we need to compare self.prompt + self.generated to request['tokens']
+                # and strip generated/cache if needed
+                new_tokens = request['tokens'][:]
+                
+                match_len = _longest_prefix(self.tokens, new_tokens)
 
-        if self.session_id is None or self.session_id != request['session_id']:
-            # starting new session
-            self.session_id = request['session_id']
-            logging.info(f"starting session {self.session_id}")
-            self.tokens = request['tokens'][:]
-            self.cache = [None for _ in self.model.layers]
-            self.cache_len = 0
-        else:
-            # we need to compare self.prompt + self.generated to request['tokens']
-            # and strip generated/cache if needed
-            new_tokens = request['tokens'][:]
-            
-            match_len = _longest_prefix(self.tokens, new_tokens)
+                # if everything matched. We can keep all generated + cache, 
+                # even what we have generated after the new prompt. Otherwise:
 
-            # if everything matched. We can keep all generated + cache, 
-            # even what we have generated after the new prompt. Otherwise:
+                if match_len < len(new_tokens):
 
-            if match_len < len(new_tokens):
+                    # and the generated tokens we need to update to all the passed tokens
+                    self.tokens = new_tokens
 
-                # and the generated tokens we need to update to all the passed tokens
-                self.tokens = new_tokens
-
-                for i in range(len(self.cache)):
-                    # either all caches are empty, or all are not
-                    if self.cache[i] is None:
-                        break
-                    K = self.cache[i][0][:, :, :match_len, :]
-                    V = self.cache[i][1][:, :, :match_len, :]
-                    self.cache[i] = K, V
-                # TODO: can this be off by 1?
-                # we can keep the cache up to match_len. 
-                # Or should it be match len - 1? no, we'll compute the same thing again anyway
-                self.cache_len = match_len
+                    for i in range(len(self.cache)):
+                        # either all caches are empty, or all are not
+                        if self.cache[i] is None:
+                            break
+                        K = self.cache[i][0][:, :, :match_len, :]
+                        V = self.cache[i][1][:, :, :match_len, :]
+                        self.cache[i] = K, V
+                    # TODO: can this be off by 1?
+                    # we can keep the cache up to match_len. 
+                    # Or should it be match len - 1? no, we'll compute the same thing again anyway
+                    self.cache_len = match_len
                 
     def gen_next(self):
         if self.session_id is None:
             return
-        # need to find the input. It is a difference between populated to cache and current tokens
-        tokens_to_process = self.tokens[self.cache_len:]
+        with ft.Timer("gen_next_latency") as _:
+            with ft.Timer("inference_latency") as _:
+                # need to find the input. It is a difference between populated to cache and current tokens
+                tokens_to_process = self.tokens[self.cache_len:]
 
-        x = mx.array(tokens_to_process)[None]
-        with ft.Timer("model_eval_latency") as _:
-            logits, local_cache = self.model(x, self.cache)
-        y = mx.argmax(logits[:, -1, :]).item()
-        self.tokens.append(y)
+                x = mx.array(tokens_to_process)[None]
+                with ft.Timer("model_eval_latency") as _:
+                    logits, local_cache = self.model(x, self.cache)
+                    y = mx.argmax(logits[:, -1, :]).item()
+                self.tokens.append(y)
 
-        for i, (local_K, local_V) in enumerate(local_cache):
-            assert local_V.shape[2] == len(tokens_to_process)
-            assert local_K.shape[2] == len(tokens_to_process)
-            
-            if self.cache[i] is not None:
-                K, V = self.cache[i]
-                K = mx.concatenate([K, local_K], axis=2)
-                V = mx.concatenate([V, local_V], axis=2)
-                self.cache[i] = K, V
-            else:
-                self.cache[i] = local_K, local_V
-
-        self.cache_len += len(tokens_to_process)
+            with ft.Timer("cache_update_latency") as _:
+                for i, (local_K, local_V) in enumerate(local_cache):
+                    assert local_V.shape[2] == len(tokens_to_process)
+                    assert local_K.shape[2] == len(tokens_to_process)
+                    
+                    if self.cache[i] is not None:
+                        K, V = self.cache[i]
+                        K = mx.concatenate([K, local_K], axis=2)
+                        V = mx.concatenate([V, local_V], axis=2)
+                        self.cache[i] = K, V
+                    else:
+                        self.cache[i] = local_K, local_V
+                self.cache_len += len(tokens_to_process)
 
 class AsyncSpeculator:
     def __init__(self, speculator):
@@ -142,15 +143,16 @@ class AsyncSpeculator:
                 req = None
 
             if req is not None:
-                logging.info(f'working on {req}')
-                min_tokens = req.get('min_tokens', default_min_tokens)
-                self.speculator.handle_query(req)
-                while len(self.speculator.tokens) < len(req['tokens']) + min_tokens:
-                    self.speculator.gen_next() 
-                self.new_tokens = self.speculator.tokens[len(req['tokens']) - 1:]
-                logging.info(f'generated tokens: {self.speculator.tokens, self.new_tokens}')
-                self.gen_since_last = 0
-                self.queue.task_done()
+                with ft.Timer("request_handle_latency") as _:
+                    logging.info(f'working on {req}')
+                    min_tokens = req.get('min_tokens', default_min_tokens)
+                    self.speculator.handle_query(req)
+                    while len(self.speculator.tokens) < len(req['tokens']) + min_tokens:
+                        self.speculator.gen_next() 
+                    self.new_tokens = self.speculator.tokens[len(req['tokens']) - 1:]
+                    logging.info(f'generated tokens: {self.speculator.tokens, self.new_tokens}')
+                    self.gen_since_last = 0
+                    self.queue.task_done()
             else:
                 if self.gen_since_last < 16:
                     self.speculator.gen_next()
@@ -186,8 +188,8 @@ class SpeculatorHTTPHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
-        self.wfile.writelines(f'{l}\n'.encode() for l in fd.histograms('*latency*'))
-        self.wfile.writelines(f'{l}\n'.encode() for l in fm.histogram("tokens_to_process"))
+        self.wfile.writelines(f'{l}\n'.encode() for l in fd.dashboard({"charts": [('*latency', 'histogram')], "n_lines": 1}))
+        #self.wfile.writelines(f'{l}\n'.encode() for l in fm.histogram("tokens_to_process"))
 
 
 class SpeculatorHTTPServer(HTTPServer):
